9b7119340356a2c8974080edf15062123ab228a8
Martin <mth.kunze@gmail.com>
2018-03-09 20:14:16 +0100

    Update install_kube_on_arm.sh

--- install_kube_on_arm.sh
+++ install_kube_on_arm.sh
@@ -1,5 +1,6 @@
-
-
-
-
-curl --cacert /etc/kubernetes/pki/ca.crt --cert /etc/kubernetes/pki/apiserver-kubelet-client.crt --key /etc/kubernetes/pki/apiserver-kubelet-client.key https://192.168.13.1:6443
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+sudo usermod -aG docker pi
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y kubeadm kubelet
