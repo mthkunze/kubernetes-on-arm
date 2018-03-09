f056bcc702361939c78b77611e4183290e7b90cc
Martin <mth.kunze@gmail.com>
2018-03-09 20:13:20 +0100

    Update prep_kubeadm.sh

--- prep_kubeadm.sh
+++ prep_kubeadm.sh
@@ -12,3 +12,6 @@ swapoff -a
 free -m
 kubeadm reset
 kubeadm init --pod-network-cidr=10.244.0.0/16 #   --skip-preflight-checks
+
+curl --cacert /etc/kubernetes/pki/ca.crt --cert /etc/kubernetes/pki/apiserver-kubelet-client.crt --key /etc/kubernetes/pki/apiserver-kubelet-client.key https://192.168.1.1:6443
+
