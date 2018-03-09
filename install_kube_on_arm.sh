72bfb77d28d0e0b17ddc7589d8e51d5d5f9aeb3a
Martin <mth.kunze@gmail.com>
2018-03-09 20:12:16 +0100

    Create install_kube_on_arm.sh

---
+++ install_kube_on_arm.sh
@@ -0,0 +1,5 @@
+
+
+
+
+curl --cacert /etc/kubernetes/pki/ca.crt --cert /etc/kubernetes/pki/apiserver-kubelet-client.crt --key /etc/kubernetes/pki/apiserver-kubelet-client.key https://192.168.13.1:6443
