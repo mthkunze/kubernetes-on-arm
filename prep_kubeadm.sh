a4a724ae2515ad41e1203fd749224d5c24a955a6
Martin <mth.kunze@gmail.com>
2018-03-09 19:01:37 +0100

    Update prep_kubeadm.sh
    cleanup steps prep

--- prep_kubeadm.sh
+++ prep_kubeadm.sh
@@ -1,7 +1,14 @@
 #/bin/sh
 
+docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+docker rmi $(docker images | awk '/ / { print $3 }')
+
+echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' > /etc/systemd/system/kubelet.service.d/90-local-extras.conf
+systemctl daemon-reload
+systemctl restart kubelet
+
 iptables -F
 swapoff -a
 free -m
 kubeadm reset
-kubeadm init --pod-network-cidr=10.244.0.0/16
+kubeadm init --pod-network-cidr=10.244.0.0/16 #   --skip-preflight-checks
